ca110fc32b30db5ea935764139d41cafb081bdc0
SashaHrzg <71792090+SashaHrzg@users.noreply.github.com>
2022-07-17 00:08:32 +0200

    SFX

--- Assets/Scenes/Player.gd
+++ Assets/Scenes/Player.gd
@@ -45,6 +45,7 @@ func _physics_process(delta):
 	
 	#inital jump
 	var is_grounded = $FloorCheck.is_colliding()
+	
 	if can_jump:
 		if is_grounded:
 			if Input.is_action_just_pressed("jump"):
@@ -57,3 +58,11 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("right"):
 		angular_velocity.z -= rolling_force*delta
 	
+
+var play_audio_factor = 3
+
+func _on_RigidBody_body_entered(body):
+	if abs(self.linear_velocity.x) > play_audio_factor || abs(self.linear_velocity.y) > play_audio_factor || abs(self.linear_velocity.z) > play_audio_factor:
+		$"AudioStreamPlayer".play()
+
+
